a3fb2f537b15f7af55e4c7d2aca25f086643eeaf
Matheus <matheus.mvo10@gmail.com>
2017-07-22 14:56:48 -0300

    Removing datamapper dependencies from model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
-class user
-  include DataMapper::Resource
-    property :id,           Serial    # An auto-increment integer key
-    property :name,      String    # A varchar type string, for short strings
-    property :description,       Text      # A text block, for longer string data.
-    property :created_at, DateTime  # A DateTime, for any date you might like.
+class User < ActiveRecord::Base
+  # Remember to create a migration!
 end
